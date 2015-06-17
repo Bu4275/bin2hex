0c1670f2968d87867dbd7fbb5152fcbf36bfa876
Bubu <u9924050@nkfust.edu.tw>
2015-06-17 10:10:40 +0800

    add bin2hex.py

---
+++ Bin2Hex.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#coding:utf-8
+import sys
+path = sys.argv[0]
+
+line = 20
+f = open(path,"rb")
+
+print 'char shellcode[]='
+sys.stdout.write('\"')
+for b in f.read():
+    sys.stdout.write('\\x{:02x}'.format(ord(b)))
+    line -=1
+    if line == 0:
+        print '\"'
+        sys.stdout.write('\"')
+        line = 20
+
+
+sys.stdout.write('\"');
+f.close()
